4c166a5433403468c08f2eb7ca39c7b7a9853a4e
saksham-v25 <vishwakarmasaksham07@gmail.com>
2026-06-13 21:00:47 +0530

    Add files via upload

---
+++ Leetcode/Slow & Fast pointer/18_Linked_List_Cycle_II_142.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+from typing import Optional
+
+class Solution:
+    def detectCycle(self, head):
+        slow=head
+        fast=head
+        while fast!=None and fast.next!=None :
+
+            slow=slow.next
+            fast=fast.next.next
+            
+            if slow==fast:
+                slow=head
+                
+                while slow!=fast:
+                    slow=slow.next
+                    fast=fast.next
+
+                return slow
+
+        
